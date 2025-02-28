5874612cb50c6649e8f7d0b253bd67eaeeb98b03
mbert30 <bert.matheo2521@gmail.com>
2025-02-28 11:08:28 +0100

    Setup du projet : Création de l'arborescence, installation des librairies et création de Json-server+Json-server-auth

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react/jsx-runtime'
+import './App.css'
+
+function App() {
+  return (
+    <Fragment>
+      <div>Projet BlueSky</div>
+    </Fragment>
+  )
+}
+
+export default App
